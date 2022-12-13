b25dd5dada5c850e3462a51def8b14455371f28c
Joseph Vermeil <joseph.vermeil@espci.fr>
2022-12-13 13:44:57 +0100

    Reformatted images from Valentin

---
+++ Code_Python/UtilityScripts/ModifyFileNames.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 16:12:49 2022
+
+@author: JosephVermeil
+"""
+
+import os
+import re
+import pandas as pd
+dateFormat1 = re.compile('\d{2}-\d{2}-\d{2}')
+#os.rename(r'file path\OLD file name.file type',r'file path\NEW file name.file type')
+
+
+# %% Functions
+
+
+def inverseDate(path, target='file', test = True, recursiveAction = False, exceptStrings = []):
+    listAll = os.listdir(path)
+    listFiles = []
+    listDir = []
+    listTarget = []
+    for f in listAll:
+        if os.path.isfile(os.path.join(path,f)):
+            listFiles.append(f)
+        elif os.path.isdir(os.path.join(path,f)):
+            listDir.append(f)
+    if target == 'file':
+        listTarget = listFiles
+    elif target == 'dir':
+        listTarget = listDir
+    elif target == 'all':
+        listTarget = listAll
+    renamedListTarget = []
+    for f in listTarget:
+        searchDate = re.search(dateFormat1, f)
+        if searchDate:
+            doExcept = False
+            for s in exceptStrings:
+                if s in f:
+                    doExcept = True
+                    print('Exception for ' + os.path.join(path,f))
+            if not doExcept:
+                foundDate = f[searchDate.start():searchDate.end()]
+                newDate = foundDate[-2:] + foundDate[2:-2] + foundDate[:2]
+                newFileName = f[:searchDate.start()] + newDate + f[searchDate.end():]
+                if newDate[:2] not in [str(i) for i in range(18, 23)]:
+                    print("newDate = " + newDate)
+                    print('error detected in : ' + os.path.join(path,f))
+                else:
+                    renamedListTarget.append(newFileName)
+                if not test:
+                    os.rename(r''+os.path.join(path,f),r''+os.path.join(path,newFileName))
+                    
+    if recursiveAction:
+        # Update ListDir after potential renaming
+        listAll = os.listdir(path)
+        listDir = []
+        for f in listAll:
+            if os.path.isdir(os.path.join(path,f)):
+                listDir.append(f)
+        # Start going recursive
+        for d in listDir:
+            print("Let's go into " + os.path.join(path,d))
+            inverseDate(os.path.join(path,d), target, test = test, recursiveAction = True, exceptStrings = exceptStrings)
+    print(renamedListTarget)
+    
+    
+def inverseDateInsideFile(path, test = True):
+    f = open(path, 'r')
+    data = f.read()
+    searchedDates = re.findall(dateFormat1, data)
+    for date in searchedDates:
+        foundDate = date
+        newDate = foundDate[-2:] + foundDate[2:-2] + foundDate[:2]
+        data = data.replace(foundDate, newDate)
+        if newDate[:2] != '20' and newDate[:2] != '21':
+            print('error detected with date : ' + foundDate)
+    f.close()
+    print(data)
+    if not test:
+        f = open(path, 'w')
+        f.write(data)
+        f.close()
+
+# %% Script
+        
+inverseDate('D://MagneticPincherData//Raw_DC//Raw_DC_JV//', target = 'all', 
+            test = True, recursiveAction = True, exceptStrings = ['Deptho'])
+
+
+
+
+
+
+
+
---
+++ Code_Python/UtilityScripts/ModifyImageFiles.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 16:12:49 2022
+
+@author: JosephVermeil
+"""
+
+import os
+import re
+import time
+import pandas as pd
+import numpy as np
+from skimage import io
+dateFormat1 = re.compile('\d{2}-\d{2}-\d{2}')
+
+# Local Imports
+
+import sys
+import CortexPaths as cp
+sys.path.append(cp.DirRepoPython)
+sys.path.append(cp.DirRepoPythonUser)
+import GraphicStyles as gs
+# import GlobalConstants as gc
+# import UtilityFunctions as ufun
+
+# 2. Pandas settings
+pd.set_option('mode.chained_assignment',None)
+
+# 3. Graphical settings
+gs.set_default_options_jv()
+
+
+# %% Functions
+
+def isWithin(x, y, a):
+    """
+    Returns if x is in [y-a, y+a], boundaries included.
+    Works with numbers and numpy arrays.
+    """
+    return(np.abs(x-y) <= a)
+
+def formatImageFilesFromValentin(srcPath, dstPath, tag = ''):
+    expType = 'R80'
+    list_all_files = os.listdir(srcPath)
+    list_selected_files = [f for f in list_all_files if ((tag in f) and (expType in f))]
+    for f in list_selected_files:
+        if f.endswith('.tif'):
+            T0 = time.time()
+            f_s = f.split('.')
+            f0 = ''.join([str(s) for s in f_s[:-1]])
+            f_I = f0 + '.tif'
+            f_F = f0 + '_Field.txt'
+            f_R = f0 + '_Results.txt'
+            p_I, p_F, p_R = os.path.join(srcPath, f_I), os.path.join(srcPath, f_F), os.path.join(srcPath, f_R)
+            
+            print(f0)
+            
+            ps_I, ps_F, ps_R = os.path.join(dstPath, f_I), os.path.join(dstPath, f_F), os.path.join(dstPath, f_R)
+            all_done = (os.path.isfile(ps_I) and os.path.isfile(ps_F) and os.path.isfile(ps_R))
+            
+            if not all_done and os.path.isfile(p_F) and os.path.isfile(p_R):
+                dfF_raw = pd.read_csv(p_F, sep = '\t', header = None, names=['field', 'T_raw'])
+                dfR_raw = pd.read_csv(p_R, sep = '\t', header=0)
+                dfR_raw = dfR_raw.drop(columns=dfR_raw.columns[0])
+                I = io.imread(p_I)
+                nz, ny, nx = I.shape
+                print(dfF_raw.shape, I.shape)
+                # out = (dfF, dfR)
+                
+                T_raw = dfF_raw.T_raw.values
+                dT_raw = np.diff(T_raw)
+                
+                removeFirst = (dT_raw[0] > 535)
+                
+                tripletTemplate = np.array([520, 40, 40])
+                correctLastTriplet = not np.all(isWithin(dT_raw[-3:], tripletTemplate, 10))
+                
+                print(removeFirst, correctLastTriplet)
+                
+                if removeFirst:
+                    dfF = dfF_raw.drop(0, axis = 0)
+                    dfR = dfR_raw.drop(dfR_raw[dfR_raw['Slice'] == 1].index, axis = 0)
+                    dfR['Slice'] = dfR['Slice'] - 1
+                    dfR = dfR.reset_index(drop=True)
+
+                    
+                else:
+                    dfF = dfF_raw.loc[:,:]
+                    dfR = dfR_raw.loc[:,:]
+                    
+                if correctLastTriplet:
+                    idx = np.argmax(dT_raw[-3:] > 500)
+                    
+                    def add_fake_timepoint_at_end(dfF, N=1):
+                        for i in range(N):
+                            B_last, T_last = dfF.field.values[-1], dfF.T_raw.values[-1]
+                            row_last = pd.DataFrame([[B_last, T_last+40]], columns=['field', 'T_raw'])
+                            dfF = pd.concat([dfF, row_last])
+                            dfF = dfF.reset_index(drop=True)
+                        return(dfF)
+                    
+                    if idx == 1: # dT_raw[-3:] = [40, 520, 40], one image is missing
+                        dfF = add_fake_timepoint_at_end(dfF, N=1)
+                        
+                    elif idx == 2: # dT_raw[-3:] = [40, 40, 520], two images are missing
+                        dfF = add_fake_timepoint_at_end(dfF, N=2)
+                        
+                # print(dfF.shape, I.shape)
+                
+                # A = np.zeros(dfF.shape[0])
+                # A[1:] = np.diff(dfF.T_raw.values)
+                # dfF['dT'] = A
+                # out = (dfF, I[-1,:,:])
+                
+                # ps_I, ps_F, ps_R = os.path.join(dstPath, f_I), os.path.join(dstPath, f_F), os.path.join(dstPath, f_R)
+                
+                if not os.path.isfile(ps_I):
+                    io.imsave(ps_I, I)
+                if not os.path.isfile(ps_F):
+                    dfF.to_csv(ps_F, sep='\t', header = False, index = False)
+                if not os.path.isfile(ps_R):
+                    dfR.to_csv(ps_R, sep='\t', header = True, index = True)
+                
+                print(dfF.shape[0]/179, I.shape[0]/179)
+                
+            T1 = time.time()
+            print(gs.GREEN + 'T = {:.3f}'.format(T1 - T0) + gs.NORMAL)
+    
+
+
+# %% Script
+        
+srcDir0 = "D://MagneticPincherData//Raw"
+dates = ['18.08.28', '18.09.24', '18.09.25', '18.10.30', '18.12.12']
+tag = ''
+
+for d in dates[:]:
+    srcDir = os.path.join(srcDir0, d + '_Raw')
+    dstDir = os.path.join(srcDir0, d)
+    out = formatImageFilesFromValentin(srcDir, dstDir, tag = tag)
+
+# dirTest = "D://MagneticPincherData//Raw_DC//Test"
+# CellIdTest = '12-12-18_M1_P1_C5'
+
+# out = formatImageFilesFromValentin(dirTest, tag = CellIdTest)
+
+
